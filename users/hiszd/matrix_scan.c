7a1039f215b94174b2b7522c73cab6a45a6bc143
Zion Koyl <hiszd1@gmail.com>
2022-11-08 13:25:55 -0500

    added my layouts

---
+++ users/hiszd/matrix_scan.c
@@ -0,0 +1,19 @@
+#include "hiszd.h"
+
+__attribute__((weak)) bool matrix_scan_user_keymap(void) {
+    return true;
+}
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_ONE_KEY(KC_F) {
+      // Anything you can do in a macro.
+      SEND_STRING("QMK is awesome.");
+    }
+  }
+}
---
+++ users/hiszd/matrix_scan.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "hiszd.h"
+
+bool matrix_scan_user_keymap(void);

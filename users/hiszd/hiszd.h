bec15421db4f129aa84ff2782a4e73c7e509427f
Zion Koyl <hiszd1@gmail.com>
2023-03-23 21:29:59 -0400

    changed rstlne layout

--- users/hiszd/hiszd.h
+++ users/hiszd/hiszd.h
@@ -80,13 +80,13 @@ uint16_t encoder_rotated_timer;
 #define __________________QWERTY_R2___________________ KC_H, KC_J, KC_K, KC_L, KC_SCLN
 #define __________________QWERTY_R3___________________ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH
 
-#define __________________RSTLNE_L1___________________ KC_Q, KC_W, KC_D, KC_F, KC_G
+#define __________________RSTLNE_L1___________________ KC_Q, KC_W, KC_D, KC_F, KC_Z
 #define __________________RSTLNE_L2___________________ KC_A, KC_S, KC_E, KC_R, KC_T
-#define __________________RSTLNE_L3___________________ KC_Z, KC_X, KC_C, KC_V, KC_SLSH
+#define __________________RSTLNE_L3___________________ KC_G, KC_X, KC_C, KC_V, KC_SLSH
 
 #define __________________RSTLNE_R1___________________ KC_SCLN, KC_U, KC_K, KC_Y, KC_P
-#define __________________RSTLNE_R2___________________ KC_H, KC_N, KC_I, KC_O, KC_L
-#define __________________RSTLNE_R3___________________ KC_B, KC_J, KC_M, KC_COMM, KC_DOT
+#define __________________RSTLNE_R2___________________ KC_H, KC_N, KC_I, KC_O, KC_J
+#define __________________RSTLNE_R3___________________ KC_B, KC_L, KC_M, KC_COMM, KC_DOT
 
 #define __________________NUM_LEFT____________________ KC_1, KC_2, KC_3, KC_4, KC_5
 #define __________________NUM_RIGHT___________________ KC_6, KC_7, KC_8, KC_9, KC_0
